a5e83ab07ed1f08cb14b155d32ee7f7a80fb08cf
CrookedYoung13 <basta.ost@gmail.com>
2020-09-16 19:23:16 +0300

    ponyav

---
+++ laba 2/laba 2(zavd 2)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace laba_2_zavd_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
