c387b4cb48cd9bf81ad03c7cb3cb1777ddb3c477
CrookedYoung13 <basta.ost@gmail.com>
2020-10-28 22:18:10 +0200

    +10

---
+++ laba 2/010/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int n = input.Length;
+            double difference = double.Parse(Console.ReadLine().Replace('.', ','));
+
+            double[] numbers = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = double.Parse(input[i].Replace('.', ','));
+            }
+
+            int count = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (Math.Abs(numbers[i] - numbers[j]) == difference)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ laba 2/02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int k = int.Parse(Console.ReadLine());
+            int arraySize = input.Length;
+
+            int[] numbers = new int[arraySize];
+            for (int i = 0; i < arraySize; i++)
+            {
+                numbers[i] = int.Parse(input[i]);
+            }
+
+            int[] sum = new int[arraySize];
+            for (int r = 1; r <= k; r++)
+            {
+                for (int i = 0; i <= arraySize - 1; i++)
+                {
+                    sum[(i + r) % arraySize] += numbers[i];
+                }
+            }
+
+            foreach (int item in sum)
+            {
+                Console.Write("{0} ", item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
